28944dfa2bd017c98061031072c8020c954adc3c
mhmdrz22 <mhmdrdaqnbry2025@gmail.com>
2025-12-31 12:27:27 +0000

    Create exceptions.py

---
+++ backend/accounts/exceptions.py
@@ -0,0 +1,10 @@
+﻿from rest_framework.exceptions import APIException
+
+
+class CustomValidationException(APIException):
+    status_code = 400
+    default_detail = "Invalid input."
+    default_code = "invalid"
+
+
+__all__ = ["CustomValidationException"]
