3c148b32ad97e6a03d0c8c5f1fbe16af0c3f411f
mhmdrz22 <mhmdrdaqnbry2025@gmail.com>
2025-12-31 14:53:38 +0330

    feat: add User admin configuration

---
+++ backend/apps/accounts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# Re-register UserAdmin
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined']
+    list_filter = ['is_staff', 'is_active', 'date_joined']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
