35bc9fa0b13a4c9639964530597ae678a001c9c1
mhmdrz22 <mhmdrdaqnbry2025@gmail.com>
2025-12-31 16:34:04 +0330

    Delete backend/adminpanel directory

--- backend/adminpanel/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AdminOverviewView, AdminNotifyView
-
-urlpatterns = [
-    path("overview/", AdminOverviewView.as_view(), name="admin-overview"),
-    path("notify/", AdminNotifyView.as_view(), name="admin-notify"),
-]
