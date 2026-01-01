b8e777a83888130a1a6be513f8a3e566e77a107b
mhmdrz22 <mhmdrdaqnbry2025@gmail.com>
2026-01-01 22:18:56 +0330

    Add complete accounts tests and admin from software-project-guilance

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,10 +1,45 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Re-register UserAdmin
+User = get_user_model()
+
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined']
-    list_filter = ['is_staff', 'is_active', 'date_joined']
-    search_fields = ['username', 'email', 'first_name', 'last_name']
+    ordering = ["-created_date"]
+    list_display = ["email", "username", "is_staff", "is_active", "is_verified", "created_date"]
+    list_filter = ["is_staff", "is_active", "is_verified"]
+    search_fields = ["email", "username"]
+    
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("username",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_verified",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "created_date", "updated_date")}),
+    )
+    
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
+    
+    readonly_fields = ["created_date", "updated_date", "last_login"]
