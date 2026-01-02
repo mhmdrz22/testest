cb56dc80360b62ec538522e129915126777152a6
mhmdrz22 <mhmdrdaqnbry2025@gmail.com>
2026-01-02 09:41:26 +0330

    Update exceptions.py

--- backend/accounts/exceptions.py
+++ backend/accounts/exceptions.py
@@ -1,8 +1,10 @@
-from rest_framework.exceptions import APIException
-from rest_framework import status
+﻿from rest_framework.exceptions import APIException
 
 
-class CustomValidationError(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = 'Validation error.'
-    default_code = 'validation_error'
+class CustomValidationException(APIException):
+    status_code = 400
+    default_detail = "Invalid input."
+    default_code = "invalid"
+
+
+__all__ = ["CustomValidationException"]
